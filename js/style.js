a3cb80aee6f22dd6a6e88f40b344400f42dc79f0
Your Name <sr.hong0114@gmail.com>
2023-05-01 17:48:14 +0900

    first commit

---
+++ js/style.js
@@ -0,0 +1,11 @@
+﻿ function toggleBtn(){
+	 const btn = document.getElementById("btn");
+	 
+	 // bnt 숨기기 (display: non)
+	 
+	 if(btn.style.display !== "none") {btn.style.display = "none";}
+	 
+	 // bnt 보이기 	 (display: block)
+	 
+	 else{btn.style.display = "block";}
+ }
